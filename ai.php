c39c77b4fad7fcc90224c72e127918765100b1de
Török Viktor <torokv@vnet.hu>
2017-11-02 19:33:53 +0100

    Creating page structure

---
+++ ai.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="main.css" />
+        <title>Török Viktor (N1EEZB) honlapja</title>            
+    </head>
+    
+    <body>
+        <header>
+            Török Viktor honlapja
+        </header>
+        
+        <nav>
+            <ul>
+                <li><a href="index.php">Főoldal</a></li>
+                <li><a href="#">Mesterséges intelligencia</a></li>
+                <li><a href="#">PIC programozás</a></li>
+                <li><a href="#">Linkek</a></li>
+                <li><a href="#">Hírlevél</a></li>
+            </ul>
+        </nav>
+        
+        <article>
+            <h1>Mi a mesterséges intelligencia?</h1>
+            
+            <img src="images/robot.png" />
+            
+            <p>
+                A mesterséges intelligencia (MI), vagy angolul az artificial intelligence (AI) meghatározása nem egyszerű kérdés.
+                Maga a kifejezés először 1956-ban jelent meg, John McCarthy számítógépes kutató használta az elnevezést a dartmouthi
+                konferencián.
+            </p>
+            
+            <p>
+                A szakirodalomban többféle megközelítéssel találkozhatunk, amelyek messze nem egységesek. Nézzünk meg néhány lehetséges
+                definíciót!
+            </p>
+            
+            <p>
+                Az Oxford számítástechnikai értelmező szótár szerint a mesterséges intelligencia:
+                <em>A számítástudománynak az a területe, amely emberi intelligenciát igénylő feladatokat megoldó számítógépes programok készítésével
+                foglalkozik.</em>
+            </p>
+            
+            <p>
+                A Futó Iván által szerkesztett Mesterséges intelligencia című könyvben több különböző meghatározást is olvashatunk.
+                A megfogalmazás módja attól függ, hogy mely területen dolgozó kutató próbálta meg leírni az MI fogalmát:
+            </p>
+            
+            <p>
+                Haugenland, 1985: <em>Az MI egy izgalmas erőfeszítés a számítógépek gondolkodóvá tételére, értelemmel bíró gépek
+                létrehozására a szó szoros értelmében.</em>
+            </p>
+                
+            <p>
+                Bellman, 1978: <em>Az MI az emberi gondolkodáshoz asszociált tevékenységek, mint a döntéshozatal, problémamegoldás,
+                tanulás automatizálása vizsgálata.</em>
+            </p>
+            
+            <p>
+                Charniak, 1985: <em>Az MI a mentális képességek tanulmányozása számítógépes modellek segítségével.</em>
+            </p>
+            
+            <p>
+                Winston, 1992: <em>Az MI az érzékelést, gondolkodást és cselekvést lehetővé tevő számítások (computation)
+                tanulmányozása.</em>
+            </p>
+            
+            <p>
+                Kurzweil, 1990: <em>Az MI olyan funkciók megvalósítására alkalmas gépek tudománya, mely funkciókhoz
+                intelligenciára van szükség, amennyiben azokat emberek valósítják meg.</em>
+            </p>
+            
+            <p>
+                Rich, 1991: <em>Az MI annak tanulmányozása, hogyan lehet számítógéppel olyan dolgokat tenni, melyeket
+                jelenleg az emberek jobban tudnak.</em>
+            </p>
+            
+            <p>
+                Schalkoff, 1990: <em>Az MI olyan tanulmányterület, amely számítási eljárásokkal próbálja magyarázni és utánozni
+                az intelligens viselkedést.</em>
+            </p>
+            
+            <p>
+                Luger, 1993: <em>Az MI a számítástudomány azon ága, mely az intelligens viselkedés automatizálásával foglalkozik.</em>
+            </p>
+            
+            <p>
+                A magyar Wikipedia az alábbi módon határozza meg az MI-t: <em>Mesterséges intelligenciának (MI vagy AI – az
+                angol Artificial Intelligence-ből) egy gép, program vagy mesterségesen létrehozott tudat által megnyilvánuló
+                intelligenciát nevezzük. A fogalmat legtöbbször a számítógépekkel társítjuk.</em>
+            </p>
+            
+            <p>
+                A leírtakból látható, hogy nehéz pontos definíciót adni az MI-re, az MI általánosan elfogadott,
+                egyértelmű definíciója mind a mai napig hiányzik. Maga a tudományág sok különböző területet érint,
+                pl. kereső algoritmusok, gépi tanulás, robotika, kétszemélyes játékok, alakfelismerés, beszédfelismerés,
+                intelligens ágensek stb.
+            </p>
+        </article>
+        
+        <footer>
+            &copy; 2017 Török Viktor
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="main.css" />
+        <title>Török Viktor (N1EEZB) honlapja</title>            
+    </head>
+    
+    <body>
+        <header>
+            Török Viktor honlapja
+        </header>
+        
+        <nav>
+            <ul>
+                <li><a href="index.php">Főoldal</a></li>
+                <li><a href="ai.php">Mesterséges intelligencia</a></li>
+                <li><a href="#">PIC programozás</a></li>
+                <li><a href="#">Linkek</a></li>
+                <li><a href="#">Hírlevél</a></li>
+            </ul>
+        </nav>
+        
+        <footer>
+            &copy; 2017 Török Viktor
+        </footer>
+    </body>
+</html>
